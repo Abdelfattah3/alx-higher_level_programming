c9c7e53071796efe028b0d99e8a9206059c87ab2
Abdelfattah3 <abdoselkady456@gmail.com>
2023-11-06 13:57:16 -0800

    task 10&11 edit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
 
-    if not my_list:
+    if my_list is None:
         return None
-    list_res = []
+    list_div = []
     for x in my_list:
-        if my_list[x] % 2 == 0:
-            list_res.append(True)
+        if (x % 2) == 0:
+            list_div.append(True)
         else:
-            list_res.append(False)
-    return list_res
+            list_div.append(False)
+    return list_div
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,10 +3,5 @@ def delete_at(my_list=[], idx=0):
 
     if idx < 0 or idx >= len(my_list):
         return my_list
-    if idx == 0:
-        return my_list[1:]
-    elif idx == len(my_list) - 1:
-        return my_list[:-1]
-    else:
-        del my_list[idx]
-        return my_list
+    del my_list[idx]
+    return my_list
