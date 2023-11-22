8ff165eb4228dfc873f9b8a434d1232462a76adb
Abdelfattah3 <abdoselkady456@gmail.com>
2023-11-21 16:24:46 -0800

    task 6 ?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,7 +49,7 @@ class Square:
         Returns:
             tuple : of the coordination of the square.
         """
-        return self.__position
+        return (self.__position)
 
     @size.setter
     def position(self, value):
@@ -63,11 +63,10 @@ class Square:
         """
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(i, int) for i in value) or
-                not all(i >= 0 for i in value)):
+                not all(isinstance(no, int) for no in value) or
+                not all(no >= 0 for no in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         '''Calculating the square area'''
