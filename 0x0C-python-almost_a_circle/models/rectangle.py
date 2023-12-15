cb33ddbdb731d6df07fbf6f99b9e1862e42cfcad
Abdelfattah3 <abdoselkady456@gmail.com>
2023-12-16 01:55:06 +0200

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module for Rectangle class"""
+"""Importing Base class"""
 from base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class"""
+    """Rectangle class inhert from Base class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Rectangle class initializing"""
