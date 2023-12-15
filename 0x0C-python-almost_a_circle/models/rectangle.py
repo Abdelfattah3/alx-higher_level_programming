0c821f266814cdda68bd3e81b524253a9c32f503
Abdelfattah3 <abdoselkady456@gmail.com>
2023-12-16 01:49:17 +0200

    task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Class rectangle from base class"""
+"""importing Class rectangle from base class"""
 from base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class
-    Args:
-        Base (Base): superclass
-    """
+    """Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """subclass initializing"""
         super().__init__(id)
