59886ef42095c8579a6c0b9134f75678fe85fcb0
Abdelfattah3 <abdoselkady456@gmail.com>
2024-02-19 15:17:58 +0200

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table in database on MySQL server
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
