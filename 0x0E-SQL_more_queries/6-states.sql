fb4f7275f3b4f25b7a8838b422dca8671286f445
Abdelfattah3 <abdoselkady456@gmail.com>
2024-02-21 17:14:18 +0200

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates and shows grants for users
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+);
