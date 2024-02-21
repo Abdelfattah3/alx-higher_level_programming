4d8e3bbbaeb1c5aece32ed35d479e3d2e90c8f89
Abdelfattah3 <abdoselkady456@gmail.com>
2024-02-21 16:06:25 +0200

    tsak 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates and shows grants for users
 CREATE TABLE IF NOT EXISTS id_not_null(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT DEFAULT 1,
     name VARCHAR(256)
 );
