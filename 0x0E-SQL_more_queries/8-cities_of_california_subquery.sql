c6d1056c8a848cb990eaf0f8e2cdb1ac1895ee0d
Abdelfattah3 <abdoselkady456@gmail.com>
2024-02-24 15:42:49 +0200

    task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- creates and shows grants for users
-SELECT id, name FROM cities WHERE ((SELECT id FROM states WHERE name = 'California') = state_id) ORDER BY id ASC;
+SELECT id, name FROM cities WHERE ((SELECT id FROM states WHERE ) = state_id) ORDER BY id ASC;
